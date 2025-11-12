031b3b3ed0abe64f23d4cb5d57b43d91922f3122
JohnnyGZ41 <johnnyundertale41@outlook.com>
2025-11-12 18:59:01 -0500

    update: add swagger and configuration

--- SmilingCup-Backend/obj/Debug/net9.0/SmilingCup-Backend.AssemblyInfo.cs
+++ SmilingCup-Backend/obj/Debug/net9.0/SmilingCup-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmilingCup-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6363a62c2e07db7d564a49ca612f6955e1d2f79e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67eed829aaca5145ab69d816e3c7261d03e0b1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmilingCup-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmilingCup-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
