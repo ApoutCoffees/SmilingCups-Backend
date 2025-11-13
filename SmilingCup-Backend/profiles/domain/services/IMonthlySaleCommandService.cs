86f01478d6d07fccdb403bb3fdf49a75aeccf44a
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-12 21:29:10 -0500

    feat(profiles): add command services

---
+++ SmilingCup-Backend/profiles/domain/services/IMonthlySaleCommandService.cs
@@ -0,0 +1,9 @@
+﻿using SmilingCup_Backend.profiles.domain.model.aggregates;
+using SmilingCup_Backend.profiles.domain.model.commands;
+
+namespace SmilingCup_Backend.profiles.domain.services;
+
+public interface IMonthlySaleCommandService
+{
+    Task<MonthlySale> Handle(CreateMonthlySaleCommand command);
+}
---
+++ SmilingCup-Backend/profiles/domain/services/IProducerStatCommandService.cs
@@ -0,0 +1,9 @@
+﻿using SmilingCup_Backend.profiles.domain.model.aggregates;
+using SmilingCup_Backend.profiles.domain.model.commands;
+
+namespace SmilingCup_Backend.profiles.domain.services;
+
+public interface IProducerStatCommandService
+{
+    Task<ProducerStat> Handle(CreateProducerStatCommand command);
+}
