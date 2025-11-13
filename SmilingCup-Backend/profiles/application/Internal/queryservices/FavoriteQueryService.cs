05c00e4f4736074ead5771aa1aba4e5d5cbdd2d1
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-12 23:12:19 -0500

    feat(profiles): add query service implementation

---
+++ SmilingCup-Backend/profiles/application/Internal/queryservices/FavoriteQueryService.cs
@@ -0,0 +1,20 @@
+﻿using SmilingCup_Backend.profiles.domain.model.aggregates;
+using SmilingCup_Backend.profiles.domain.model.queries;
+using SmilingCup_Backend.profiles.domain.repositories;
+using SmilingCup_Backend.profiles.domain.services;
+
+namespace SmilingCup_Backend.profiles.application.Internal.queryservices;
+
+public class FavoriteQueryService(IFavoriteRepository favoriteRepository)
+    : IFavoriteQueryService
+{
+    public async Task<IEnumerable<Favorite>> Handle(GetAllFavoritesQuery query)
+    {
+        return await favoriteRepository.ListAsync();
+    }
+
+    public async Task<Favorite?> Handle(GetFavoriteByIdQuery query)
+    {
+        return await favoriteRepository.FindByIdAsync(query.id);
+    }
+}
