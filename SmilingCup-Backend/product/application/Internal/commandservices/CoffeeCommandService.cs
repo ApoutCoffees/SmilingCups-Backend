3030e4f309399df0a9922d045df8eec0d30f2a5d
Daniel Aquino <u202217678@upc.edu.pe>
2025-11-13 10:39:21 -0500

    feat(product): add services implementation

---
+++ SmilingCup-Backend/product/application/Internal/commandservices/CoffeeCommandService.cs
@@ -0,0 +1,29 @@
+﻿using SmilingCup_Backend.product.domain.model.aggregates;
+using SmilingCup_Backend.product.domain.model.commands;
+using SmilingCup_Backend.product.domain.repositories;
+using SmilingCup_Backend.product.domain.services;
+using SmilingCup_Backend.Shared.domain.repositories;
+
+namespace SmilingCup_Backend.product.application.Internal.commandservices;
+
+public class CoffeeCommandService(
+    ICoffeeRepository coffeeRepository,
+    IUnitOfWork unitOfWork)
+    : ICoffeeCommandService
+{
+    public async Task<Coffee?> Handle(CreateCoffeeCommand command)
+    {
+        var coffee = new Coffee(command);
+        try
+        {
+            await coffeeRepository.AddAsync(coffee);
+            await unitOfWork.CompleteAsync();
+            return coffee;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+    
+}
